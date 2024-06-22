5bef52dee47d8615ff763824bbfebe81ec953baa
AnnulusGames <annulusgames@gmail.com>
2024-06-22 12:18:09 +0900

    Add GetFormatterWithVerify

--- src/Csv/ICsvFormatterProvider.cs
+++ src/Csv/ICsvFormatterProvider.cs
@@ -4,3 +4,13 @@ public interface ICsvFormatterProvider
 {
     public ICsvFormatter<T>? GetFormatter<T>();
 }
+
+internal static class CsvFormatterProviderExtensions
+{
+    public static ICsvFormatter<T> GetFormatterWithVarify<T>(this ICsvFormatterProvider formatterProvider)
+    {
+        var formatter = formatterProvider.GetFormatter<T>();
+        if (formatter == null) CsvSerializationException.ThrowFormatterNotRegistered(typeof(T));
+        return formatter;
+    }
+}
